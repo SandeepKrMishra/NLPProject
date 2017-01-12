4dd509d2ecd4d48ac3ebaac6d7824c7f10f0435d
sandeepKr <sandeep.mishra@datarpm.com>
2017-01-12 16:23:52 +0530

    added nlp code

---
+++ src/com/test/SampleRecommender.java
@@ -0,0 +1,37 @@
+package com.test;
+
+import java.io.File;
+import java.util.List;
+
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+
+
+public class SampleRecommender {
+	
+	public static void main(String[] args) {
+		try {
+			DataModel model = new FileDataModel(new File("/home/sandeep/dataset.csv"));
+		
+			UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+		
+			UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
+		
+			UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+		
+			List<RecommendedItem> recommendations = recommender.recommend(2, 5);
+			for (RecommendedItem recommendation : recommendations) {
+			  System.out.println(recommendation);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
